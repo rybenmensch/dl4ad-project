60601309a260671a363d62b075a6db0cbf8fd515
Manolo Müller <manolo.mueller@gmail.com>
2026-09-24 13:09:43 +0200

    Added WrapLayer

--- modules.py
+++ modules.py
@@ -3,25 +3,39 @@ from torch import Tensor
 from torch.nn import Module
 
 from model import LayerInfo
+from torch_lib import get_layer_name
 
 
-class SkippingLayer(Module):
+class WrapLayer(Module):
+    def __init__(self, layer_info: LayerInfo, args: str) -> None:
+        super().__init__()
+        self.layer = layer_info.get_layer()
+        self.__args = args
+
+    def __repr__(self) -> str:
+        layer_name = self.__class__.__name__
+        if self.__args == "":
+            args = ""
+        else:
+            args = ", " + self.__args
+        return f"{layer_name}({get_layer_name(self.layer)}{args})"
+
+
+class SkippingLayer(WrapLayer):
     """Layer that forwards its input unchanged."""
 
-    def __init__(self, layer: LayerInfo) -> None:
-        super().__init__()
-        self.layer = layer.get_layer()
+    def __init__(self, layer_info: LayerInfo) -> None:
+        super().__init__(layer_info, "")
 
     def forward(self, x: Tensor) -> Tensor:
         return x
 
 
-class RepeatingLayer(Module):
+class RepeatingLayer(WrapLayer):
     """Layer that applies its processing multiple times."""
 
-    def __init__(self, layer: LayerInfo, repeats: int = 1) -> None:
-        super().__init__()
-        self.layer = layer.get_layer()
+    def __init__(self, layer_info: LayerInfo, repeats: int = 1) -> None:
+        super().__init__(layer_info, str(repeats))
         self.repeats = repeats
 
     def forward(self, x: Tensor) -> Tensor:
@@ -30,16 +44,27 @@ class RepeatingLayer(Module):
         return x
 
 
-class MultiplierLayer(Module):
+def make_w_b_string(w: float, b: float, default: int) -> str:
+    if b == default:
+        if w == default:
+            return ""
+        return f"{w=}"
+    return f"{w=}, {b=}"
+
+
+class MultiplierLayer(WrapLayer):
     """Layer that multiplies weights and biases."""
 
     def __init__(
-        self, layer: LayerInfo, weight_mul: float = 1, bias_mul: float = 1
+        self, layer_info: LayerInfo, weight_mul: float = 1, bias_mul: float = 1
     ) -> None:
-        super().__init__()
-        self.layer = layer.get_layer()
+        super().__init__(layer_info, make_w_b_string(weight_mul, bias_mul, 1))
+
+        self.weight_mul = weight_mul
+        self.bias_mul = bias_mul
+
         with torch.no_grad():
-            for w_b in layer.model.layer_get_weight_and_bias(self.layer):
+            for w_b in layer_info.model.layer_get_weight_and_bias(self.layer):
                 w_b.weight.mul_(weight_mul)
                 w_b.bias.mul_(bias_mul)
 
@@ -47,16 +72,19 @@ class MultiplierLayer(Module):
         return self.layer(x)
 
 
-class AdditionLayer(Module):
+class AdditionLayer(WrapLayer):
     """Layer that adds an offset to weights and biases."""
 
     def __init__(
-        self, layer: LayerInfo, weight_add: float = 1, bias_add: float = 1
+        self, layer_info: LayerInfo, weight_add: float = 1, bias_add: float = 1
     ) -> None:
-        super().__init__()
-        self.layer = layer.get_layer()
+        super().__init__(layer_info, make_w_b_string(weight_add, bias_add, 0))
+
+        self.weight_add = weight_add
+        self.bias_add = bias_add
+
         with torch.no_grad():
-            for w_b in layer.model.layer_get_weight_and_bias(self.layer):
+            for w_b in layer_info.model.layer_get_weight_and_bias(self.layer):
                 w_b.weight.add_(weight_add)
                 w_b.bias.add_(bias_add)
 
